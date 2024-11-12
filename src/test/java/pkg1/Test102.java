0598bb7ddae44f7d75b660dbb44ec0e31de71077
Sankhadeep <sankha.cse@gmail.com>
2024-11-12 21:30:33 +0530

    Test101 file updated. Test102 file created.

--- src/test/java/pkg1/Test102.java
+++ src/test/java/pkg1/Test102.java
@@ -1,11 +1,10 @@
 package pkg1;
 
-public class Test101 {
+public class Test102 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("sample statement - 101");
-
+		System.out.println("sample statement - 102");
 	}
 
 }
