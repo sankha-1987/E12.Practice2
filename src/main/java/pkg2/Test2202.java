88c108d5f3fd088960915fa4a27f224cd4e6469e
Sankhadeep <sankha.cse@gmail.com>
2024-11-12 21:46:35 +0530

    pkg2 and pkg3 packages are created in src/main/java. Test2202 and Test3303 files are created sequentially.

---
+++ src/main/java/pkg2/Test2202.java
@@ -0,0 +1,11 @@
+package pkg2;
+
+public class Test2202 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("statement - 2202");
+
+	}
+
+}
---
+++ src/main/java/pkg3/Test3303.java
@@ -0,0 +1,11 @@
+package pkg3;
+
+public class Test3303 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("statement - 3303");
+
+	}
+
+}
