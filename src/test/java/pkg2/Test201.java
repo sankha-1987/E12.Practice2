20e8fb91dc18a362cf445d03a7154b3a5a5c3c9d
Sankhadeep <sankha.cse@gmail.com>
2024-11-12 23:18:27 +0530

    Test202 created. Test201 updated.

--- src/test/java/pkg2/Test201.java
+++ src/test/java/pkg2/Test201.java
@@ -4,6 +4,6 @@ public class Test201 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("sample statement - 2");
+		System.out.println("sample statement - 201");
 	}
 }
---
+++ src/test/java/pkg2/Test202.java
@@ -0,0 +1,10 @@
+package pkg2;
+
+public class Test202 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("sample statement - 202");
+	}
+
+}
