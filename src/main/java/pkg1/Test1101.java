3a6517fc82075d7be28edc2715981079af383642
Sankhadeep <sankha.cse@gmail.com>
2024-11-13 21:14:01 +0530

    Test1101 and Test202 files updated.

--- src/main/java/pkg1/Test1101.java
+++ src/main/java/pkg1/Test1101.java
@@ -5,5 +5,6 @@ public class Test1101 {
 	public static void main(String[] args) {
 		
 		System.out.println("statement - 1101");
+		System.out.println("statement - 1101 - 2");
 	}
 }
--- src/test/java/pkg2/Test202.java
+++ src/test/java/pkg2/Test202.java
@@ -5,6 +5,6 @@ public class Test202 {
 	public static void main(String[] args) {
 		
 		System.out.println("sample statement - 202");
+		System.out.println("sample statement - 202 - 2");
 	}
-
 }
